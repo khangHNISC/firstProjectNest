6cc0eebea3871969c1a1f38ef5b94d2033337d3c
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2021-04-20 11:23:46 +0700

    nest g controller cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
 
+//The root module of the application.
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
